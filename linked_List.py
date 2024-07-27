212e657345fd3313deb28f98bd1190f4c452d470
Big Sys <Owner@MacBook-Air.lan>
2024-07-27 10:09:01 -0500

    correcting reverse method

--- linked_List.py
+++ linked_List.py
@@ -12,15 +12,16 @@ class LinkedList:
         self.length =1 
 
     def print_list(self):
-        temp = self.head 
-
-        while temp is not None: 
-            print(temp.value)
-            temp= temp.next
+        values = []
+        temp = self.head
+        while temp is not None:
+            values.append(str(temp.value))
+            temp = temp.next
+        print(" -> ".join(values))
 
     def append(self, value):
         new_node = Node(value)
-        if self.head == None:
+        if self.head is None:
             self.head = new_node
             self.tail = new_node
         else: 
@@ -35,7 +36,7 @@ class LinkedList:
         
         temp = self.head 
         pre = self.head
-        while (temp.next): 
+        while temp.next: 
             pre = temp 
             temp = temp.next 
         self.tail = pre
@@ -99,15 +100,44 @@ class LinkedList:
         return True
     
     def remove(self, index):
-           if index <0 or index >= self.length: 
+        if index <0 or index >= self.length: 
             return None 
-           temp = self.get(index)
-           prev = self.get(index - 1)
-           prev.next = temp.next 
-           temp = None
+        if index == 0: 
+            return self.pop_first()
+        if index == self.length-1:
+            return self.pop()
+        prev = self.get(index - 1)
+        temp = prev.next 
+        prev.next = temp.next 
+        temp.next = None
+        self.length-=1
+        return True
 
+    def reverse(self):
+        if self.length <= 1:
+            return
+
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        after = temp.next
+        before = None
+
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+
+        # This line is crucial - it ensures the last node (now first) points to the correct next node
+        self.head.next = before
+
+        print("Debug - Final head:", self.head.value)
+        print("Debug - Final tail:", self.tail.value)
         
 
+
+        
 
 my_linked_list = LinkedList(0)
 my_linked_list.append(2)
@@ -115,6 +145,11 @@ my_linked_list.insert(1,1)
 my_linked_list.append(3)
 my_linked_list.append(4)
 my_linked_list.append(5)
-my_linked_list.remove(3)
 
+print("Original list:")
+my_linked_list.print_list()
+
+my_linked_list.reverse()
+
+print("\nReversed list:")
 my_linked_list.print_list()
