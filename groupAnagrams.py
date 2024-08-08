eb6c26f86ff4ec3091a842337cc9ba07e5285f6c
Big Sys <Owner@macbook-air.myfiosgateway.com>
2024-08-08 18:15:44 -0400

    adding O(n) solution to topK

--- groupAnagrams.py
+++ groupAnagrams.py
@@ -1,10 +1,11 @@
+#Our task is to find all the words that are anagrams of each other and group them together.
 # Input: strs = ["act","pots","tops","cat","stop","hat"]
 
 # Output: [["hat"],["act", "cat"],["stop", "pots", "tops"]]
 from collections import defaultdict
 
 def groupAnagrams(strs):
-    #set default list to hold grouped anagrams  
+    #default provides a default value for missing keys.
     ans = defaultdict(list)
 
     #For each each str in strs, set count array to have key for each letter a -z 
--- topKFrequency.py
+++ topKFrequency.py
@@ -11,22 +11,39 @@
 from collections import Counter
 from itertools import islice
 
+#O(n) solution - bucket sort (map value to index)
+# count the occurance of each value, then pop k items 
+# make count proporortional to size of input array 
+# 
+def topKFrequency(nums, k):
+    count = {}
+    freq = [[] for i in range(len(nums) +1)]
+    for n in nums: 
+        count[n] = 1 + count.get(n, 0) 
+    for n, c in count.items():
+        freq[c].append(n)
 
+    res=[]
+    for i in range(len(freq) -1, 0, -1):
+        for n in freq[i]:
+            res.append(n)
+            if len(res) == k: 
+                return res
+
+
+
+#O(n log n)
 def topFrequency(nums, k): 
+    #make dict of numbers
     ans  = Counter(nums)
+
+    #sort dictionary based on value 
     sorted_ans = dict(sorted(ans.items(), key=lambda item: item[1], reverse=True))
 
-
-
-
-
     #make list of out of range up to K 
-   
-
     top_k_items = list(islice(sorted_ans.items(), k))
    
     output = [item[0] for item in top_k_items]
-    print("Output is: ", output)
     return output
 
 
@@ -34,5 +51,7 @@ def topFrequency(nums, k):
 
 
 
-topFrequency([1,2,2,3,3,3], k =2)
-topFrequency([1,1,1,2,3,3,3,3,3,4,5,5,6,7,8,8,8,8,8,8], k =4)
+print(topKFrequency([1,2,2,3,3,3], k =2))
+print(topFrequency([1,2,2,3,3,3], k =2))
+print(topFrequency([1,1,1,2,3,3,3,3,3,4,5,5,6,7,8,8,8,8,8,8], k =4))
+print(topKFrequency([1,1,1,2,3,3,3,3,3,4,5,5,6,7,8,8,8,8,8,8], k =4))
