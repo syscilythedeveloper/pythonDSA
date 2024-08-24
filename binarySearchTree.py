78d22ff705f0b3c4bbf03d6fe57e748da55e5c68
Syscily <Owner@macbook-air.myfiosgateway.com>
2024-08-24 09:37:44 -0400

    recursive contains

--- binarySearchTree.py
+++ binarySearchTree.py
@@ -41,6 +41,19 @@ class BinaryTree:
             else: 
                 return True 
         return False 
+    
+    def __r_contains(self, current_node, value):
+        if current_node == None: 
+            return False 
+        if value == current_node.value: 
+            return True 
+        if value < current_node.value: 
+            return self.__r_contains(current_node.left, value) 
+        if value > current_node.value: 
+            return self.__r_contains(current_node.right, value) 
+    
+    def r_contains(self, value):
+        return self.__r_contains(self.root, value)
         
 
 firstBinary = BinaryTree()
@@ -55,5 +68,8 @@ print(firstBinary.root.value)
 print(firstBinary.root.left.value)
 print(firstBinary.root.right.value)
 print(firstBinary.contains(8))
+print(firstBinary.contains(21))
+print(firstBinary.r_contains(8))
+print(firstBinary.r_contains(21))
 
 
