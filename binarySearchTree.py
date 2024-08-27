0077fae7454a0fa2989159a73e19d0ed4e69957e
Syscily <Owner@macbook-air.myfiosgateway.com>
2024-08-27 07:07:47 -0400

    bfs and dfs

--- binarySearchTree.py
+++ binarySearchTree.py
@@ -4,9 +4,38 @@ class Node:
         self.left = None 
         self.right = None 
 
-class BinaryTree: 
+class BinarySearchTree: 
     def __init__(self):
         self.root = None 
+    
+    def BFS(self):
+        current_node = self.root
+        queue = []
+        results = []
+        queue.append(current_node)
+        while len(queue) > 0: 
+            current_node = queue.pop(0)
+            results.append(current_node.value)
+            if current_node.left is not None: 
+                queue.append(current_node.left)
+            if current_node.right is not None: 
+                queue.append(current_node.right)
+        return results 
+    
+    def dfs_pre_order(self):
+        results = []
+        def traverse(current_node):
+            results.append(current_node.value)
+            if current_node.left is not None: 
+                traverse(current_node.left)
+            if current_node.right is not None: 
+                traverse(current_node.right)
+        traverse(self.root)
+        return results
+
+
+
+
      
           
 
@@ -54,22 +83,83 @@ class BinaryTree:
     
     def r_contains(self, value):
         return self.__r_contains(self.root, value)
+    
+    def __r_insert(self, current_node, value):
+        if current_node == None: 
+            return Node(value)
+        if value < current_node.value: 
+            current_node.left = self.__r_insert(current_node.left, value)
+        if value > current_node.value: 
+            current_node.right = self.__r_insert(current_node.right, value)
+        return current_node
+
+    def r_insert(self, value):
+        if self.root == None: 
+            self.root = Node(value)
+        self.__r_insert(self.root, value)
+
+    def min_value(self, current_node):
+        while (current_node.left is not None): 
+            current_node = current_node.left 
+        return current_node.value
+
+    def __delete_node(self, current_node, value):
+        if current_node == None: 
+            return None 
+        if value < current_node.value: 
+            current_node.left = self.__delete_node(current_node.left, value)
+        elif value > current_node.value: 
+            current_node.right = self.__delete_node(current_node.right, value)
+        else: 
+            if current_node.left == None and current_node.right ==None: 
+                return None
+            elif current_node.left == None: 
+                current_node = current_node.right
+            elif current_node.right == None: 
+                current_node = current_node.left
+            else: 
+                sub_tree_min = self.min_value(current_node.right)
+                current_node.value = sub_tree_min
+                current_node.right = self.__delete_node(current_node.right, sub_tree_min)
+        return current_node       
+
+    def delete_node(self, value):
+        self.__delete_node(self.root, value)
         
 
-firstBinary = BinaryTree()
+
+my_tree = BinarySearchTree()
+my_tree.r_insert(2)
+my_tree.r_insert(1)
+my_tree.r_insert(3)
+print("Root: ", my_tree.root.value)
+print("Root -> Left: ", my_tree.root.left.value)
+print("Root -> Right: ", my_tree.root.right.value)
+my_tree.delete_node(2)
+print("********Post delete *******")
+print("Root: ", my_tree.root.value)
+print("Root -> Left: ", my_tree.root.left.value)
+print("Root -> Right: ", my_tree.root.right)
+
+firstBinary = BinarySearchTree()
 firstBinary.insert(47)
 firstBinary.insert(21)
 firstBinary.insert(76)
-firstBinary.insert(84)
+firstBinary.insert(82)
+firstBinary.insert(27)
+firstBinary.insert(18)
+firstBinary.insert(52)
 firstBinary.insert(76)
-firstBinary.insert(76)
-firstBinary.insert(76)
-print(firstBinary.root.value)
-print(firstBinary.root.left.value)
-print(firstBinary.root.right.value)
-print(firstBinary.contains(8))
-print(firstBinary.contains(21))
-print(firstBinary.r_contains(8))
-print(firstBinary.r_contains(21))
+print(firstBinary.BFS())
+print(firstBinary.dfs_pre_order())
+# print(firstBinary.min_value(firstBinary.root))
+# print(firstBinary.min_value(firstBinary.root.right))
+# print(firstBinary.root.value)
+# print(firstBinary.root.left.value)
+# print(firstBinary.root.right.value)
+# print(firstBinary.contains(8))
+# print(firstBinary.contains(21))
+# print(firstBinary.r_contains(8))
+# print(firstBinary.r_contains(21))
 
 
